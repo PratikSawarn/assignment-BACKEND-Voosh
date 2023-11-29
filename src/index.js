f6322c2f81b551f73889f824ae0c5f8fd9cefb7c
Pratik Sawarn <pandeyshiv006@gmail.com>
2023-11-29 23:04:22 +0530

    Utils handlers and error handlers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv"
 import connectDb from "./db/index.js";
-
+import {app} from "./app.js"
 
 
 dotenv.config({path:'./env'})
@@ -11,6 +11,14 @@ dotenv.config({path:'./env'})
 
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on Port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("mongoDB connection failed ",err)
+})
 
 
 
