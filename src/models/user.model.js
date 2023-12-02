aaf0e65e6571d882037c53775797be64518e381e
Pratik Sawarn <pandeyshiv006@gmail.com>
2023-12-02 22:44:06 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,87 @@
+import mongoose,{Schema} from "mongoose";
+import Jwt  from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        index:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true,
+
+    },
+    avatar:{
+        type:String,//we use cloudnairy link for this
+        required:true
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:{
+        type:Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"]
+    },
+    resreshToken:{
+        type:String
+    }
+},
+{
+    timestamps:true
+})
+
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password) 
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return Jwt.sign({
+        _id : this._id,
+        email: this.email,
+        username : this.username,
+        fullname : this.fullname
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiryIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return Jwt.sign({
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiryIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
+
+export const User = mongoose.model("User",userSchema)
