bee18fa9f4274197125552153411bc6846e9e2ef
Pratik Sawarn <pandeyshiv006@gmail.com>
2023-12-23 14:06:15 +0530

    login logut functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -18,4 +19,11 @@ router.route("/register").post(
     registerUser
 )
 
-export default router
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logutUser) 
+
+
+
+export default router 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,9 +3,11 @@ import fs from "fs"  //filesystum used for node
 
           
 cloudinary.config({ 
+
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
+  
 });
 
 
